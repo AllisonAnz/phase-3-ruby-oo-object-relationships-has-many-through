2199ffea5d81321679fa5ff1d17fd91fcc1a9b47
AllisonAnz <AllisonAnzalone@gmail.com>
2021-08-29 13:13:31 -0500

    Completed Assignment

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,23 @@
+#Structure the relationship with waiter and customer by setting up our Meal class
+# Tell the meal class to know all the details of each meal instance 
+# including total cost and tip(defaults to 0)
+# also who the customer and waiter were for each meal 
+# This gives both the customer and waiter instances the ability to get all the information about the meal that they 
+# need without having to store it themselves
 class Meal
+ attr_accessor :waiter, :customer, :total, :tip
 
+  @@all = []
+
+  def initialize(waiter, customer, total, tip=0)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,41 @@
 class Waiter
+  attr_accessor :name, :yrs_experience
 
+  @@all = []
+  
+  # Each instance of Waiter class has a name & attribute describing their years of experience
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+  
+  # Method is like the one in cutomer.rb. except we use customer as a param and self refers to the instacne of Waiter
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
+  end
+
+    
+  # Build a method so waiters can find the customer that tips the best 
+  # just like in customer, waiter needs a way to get all the meals they have served 
+  # Create a #meals method again, with subtle changes 
+  def meals
+    Meal.all.select do |meal|
+      meal.waiter == self #checking for waiter now
+      end
+    end
+
+  # To find the best tipper, we can write another method #best_tipper 
+  # use the array we get from #meals, then return the customer of the meal with the highest tip 
+  def best_tipper
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+  
+    best_tipped_meal.customer
+  end
 end
